4ae384f4173eaae3604f87fa7c825562c8e5443f
Lalote23 <81178476+Nightcrawler0@users.noreply.github.com>
2023-07-19 19:17:02 +0200

    Rutas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+import { Home } from "./Home";
+import { NationalDex } from "./NationalDex";
+import { TeamBuilder } from "./TeamBuilder";
+import { Registro } from "./Registro";
+import { ListaUsuarios } from "./ListaUsuarios";
+import { LoginPage } from "./LoginPage";
+
+
+const router = createHashRouter([
+  {
+    path:'/',
+    element: <Home/>
+  },
+  {
+    path:'/NationalDex',
+    element: <NationalDex/>
+  },
+  {
+    path:'/TeamBuilder',
+    element: <TeamBuilder/>
+  },
+  {
+    path:'/GymLeaders',
+    element: <GymLeaders/>
+  },
+  {
+    path:'/ListaUsuarios',
+    element: <ListaUsuarios/>
+  },
+  {
+    path:'/Registro',
+    element: <Registro/>
+  },
+  {
+    path:'/Login',
+    element: <LoginPage/>
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
+    
   </React.StrictMode>,
 )
