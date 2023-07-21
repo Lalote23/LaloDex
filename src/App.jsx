fb42d02924edc035296b00878037afba412954b9
Lalote23 <81178476+Nightcrawler0@users.noreply.github.com>
2023-07-21 14:28:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { NationalDex } from "./NationalDex";
 import { TeamBuilder } from "./TeamBuilder";
 import { Registro } from "./Registro";
 import { ListaUsuarios } from "./ListaUsuarios";
-import { HashRouter, Link, Route, Routes,Router } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Login from "./Login";
 import { LoginPage } from "./LoginPage";
 
@@ -21,17 +21,45 @@ function App() {
     <TeamBuilder/>
     <GymLeaders/> */}
 
-<Router history={hashHistory}>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/NationalDex" element={<NationalDex />} />
-    <Route path="/TeamBuilder" element={<TeamBuilder />} />
-    <Route path="/GymLeaders" element={<GymLeaders />} />
-    <Route path="/ListaUsuarios" element={<ListaUsuarios />} />
-    <Route path="/Registro" element={<Registro />} />
-    <Route path="/Login" element={<LoginPage />} />
-  </Routes>
-</Router>
+    <BrowserRouter>
+
+
+
+        <Link to='/' ></Link>
+        <Link to='/NationalDex'></Link>
+        <Link to='/TeamBuilder'></Link>
+        <Link to='/GymLeaders'></Link>
+        <Link to='/Registro'></Link>
+        <Link to='/ListaUsuarios'></Link>
+        <Link to='/Login'></Link>
+
+
+
+
+      <Routes>
+
+      <Route path="/" element={<Home />} />
+
+      <Route path="/NationalDex" element={<NationalDex />} />
+
+      <Route path="/TeamBuilder" element={<TeamBuilder />} />
+
+      <Route path="/GymLeaders" element={<GymLeaders />} />
+
+      <Route path="/ListaUsuarios" element={<ListaUsuarios />} />
+
+      <Route path="/Registro" element={<Registro />} />
+      
+      <Route path="/Login" element={<LoginPage />} />
+
+
+
+
+
+
+      </Routes>
+
+    </BrowserRouter>
 
       </div>
     </div>
